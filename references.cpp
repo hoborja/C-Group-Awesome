c38f160318fa56a163de106be33b897ceaef823c
alno1935 <43012546+alno1935@users.noreply.github.com>
2018-11-15 11:41:03 -0500

    adjusted function greatly

--- references.cpp
+++ references.cpp
@@ -6,8 +6,26 @@ using namespace std;
 
 void references(string ref[3],int refCount) {
     for(int refCount = 0; refCount < 3; ++refCount) {
-        cout << "Please enter reference: " << refCount << endl;
-        getline(cin, ref[refCount]);
+        string name;
+        string address;
+        string phone;
+        string email;
+        string company;
+        string position;
+        cout << "Please enter reference Name: " << endl;
+        getline(cin, name);
+        cout << "Please enter the company " << name << " worked at: " << endl;
+        getline(cin, company);
+        cout << "Please enter "<< name << "'s Address: " << endl;
+        getline(cin, address);
+        cout << "Please enter "<< name << "'s phone number: " << endl;
+        getline(cin, phone);
+        cout << "Please enter "<< name << "'s Email: " << endl;
+        getline(cin, email);
+        cout << "Please enter " << name << "'s Position in " << company << ": " << endl;
+        getline(cin, position);
+        ref[refCount] = "Referance's name: " + name + "\nReference's company: " + company + "\nReference's address: " + address + 
+        "\nReference's phone number: " + phone + "\nReference's email address: " + email + "\nReference's position: " + position + "\n";
     }
 }
 
@@ -22,13 +40,14 @@ void referencesWrite(string ref[3], int refCount, ofstream& refFile) {
 
 int main(){
      ofstream refFile;
-    string ref[3] {"No References", "No References", "No References"};
-    references(ref, 3);
+    string refName[3] {"No References", "No References", "No References"};
+    string refDetails[] {"Name", "Company", "Address", "Phone", "Email", "Position"};
+    references(refName, 3);
     for(int i=0; i < 3; ++i) {
         cout <<"\n\n\n\n";
-        cout << ref[i] << endl;
+        cout << refName[i] << endl;
     }
-    referencesWrite(ref, 3, refFile);
+    referencesWrite(refName, 3, refFile);
    
 return 0;
 }
