7639834e7243e347bfb8c87707afe0a0a6e4d366
alno1935 <43012546+alno1935@users.noreply.github.com>
2018-11-15 10:30:15 -0500

    added file stream to reference code

--- references.cpp
+++ references.cpp
@@ -11,13 +11,24 @@ void references(string ref[3],int refCount) {
     }
 }
 
+void referencesWrite(string ref[3], int refCount, ofstream& refFile) {
+    for(int refCount = 0; refCount < 3; ++refCount){
+        
+        refFile.open ("test.txt", fstream::app);
+        refFile << "Reference: " << ref[refCount] << endl;
+        refFile.close();
+    }
+}
 
 int main(){
+     ofstream refFile;
     string ref[3] {"No References", "No References", "No References"};
     references(ref, 3);
     for(int i=0; i < 3; ++i) {
         cout <<"\n\n\n\n";
         cout << ref[i] << endl;
     }
+    referencesWrite(ref, 3, refFile);
+   
 return 0;
 }
